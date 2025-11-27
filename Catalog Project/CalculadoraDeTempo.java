6c9e85020055f9d5e01a562e1969ffc948a8ef84
CrisBento <crislaynnebentoufc@gmail.com>
2025-11-26 21:21:04 -0300

    Add files via upload

---
+++ Catalog Project/CalculadoraDeTempo.java
@@ -0,0 +1,19 @@
+public class CalculadoraDeTempo{
+    private int tempoTotal;
+
+    public int getTempoTotal(){
+        return this.tempoTotal;
+    }
+
+    //    public void inclui(Filme f) {
+//        this.tempoTotal += f.getDuracaoEmMinutos();
+//    }
+//
+//    public void inclui(Serie s) {
+//        this.tempoTotal += s.getDuracaoEmMinutos();
+//    }
+
+    public void inclui(Titulo titulo){
+        this.tempoTotal = tempoTotal + titulo.getDuracaoEmMinutos();
+    }
+}
